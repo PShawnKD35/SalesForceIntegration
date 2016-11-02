5244b6a77fe247d7e403636a99ca80d6a4e343a0
reasch 08 <reasch08@GMINDEX.NET>
2016-11-02 22:04:09 +0800

    Add Libs(HtmlParser and Appache HttpClient)

--- src/wzh/http/main.java
+++ src/wzh/http/main.java
@@ -1,5 +1,8 @@
 package wzh.http;
 
+import org.htmlparser.Parser;
+import org.htmlparser.util.ParserException;
+
 public class main {
 
 	public static void main(String[] args) {
@@ -11,11 +14,23 @@ public class main {
         //String sr=HttpRequest.sendPost("http://localhost:6144/Home/RequestPostString", "key=123&v=456");
         //System.out.println(sr);
 		
-		String sr=HttpRequest.sendPost("https://arms3.onezero.com/login_check", "_csrf_token=vtgcJSpa_Wi6l_WHHRhv-QZ5rUhDUUXQhiCn-KRBZc0&_username=shawn.peng%40gmimarkets.com&_password=456456&_submit=");
-        System.out.println(sr);
+//		String sr=HttpRequest.sendPost("https://arms3.onezero.com/login_check", "_csrf_token=vtgcJSpa_Wi6l_WHHRhv-QZ5rUhDUUXQhiCn-KRBZc0&_username=shawn.peng%40gmimarkets.com&_password=456456&_submit=");
+//        System.out.println(sr);
         
         String s=HttpRequest.sendGet("https://arms3.onezero.com/login", "");
 		System.out.println(s);
+		
+		try {
+			Parser parser=new Parser(s);
+			
+			
+		} catch (ParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
         
 //        String s=HttpRequest.sendGet("https://www.baidu.com/s", "ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=aaa&rsv_pq=fb3fd7960002d255&rsv_t=e005igCXF8ZJEy8ehCuX4OvIL2qLVepa0FoRFjmO2gfjWx14cIyYkRndI5g&rqlang=cn&rsv_enter=0&rsv_sug3=4&rsv_sug1=1&rsv_sug7=100&inputT=1273&rsv_sug4=1512");
 //		System.out.println(s);
