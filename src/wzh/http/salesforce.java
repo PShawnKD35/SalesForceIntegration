f4160722c62529cacc5f0a0af6f57ba58adacdbf
reasch 02 <reasch02@GMINDEX.NET>
2016-11-25 07:19:48 +0800

    edited case successfully

--- src/wzh/http/salesforce.java
+++ src/wzh/http/salesforce.java
@@ -99,104 +99,104 @@ public class salesforce {
 			List<NameValuePair> forms;
 			Header[] headers;
 			
-			// 拿登陆使用的cookie
-			httpResponse = Request.Get("https://gmi.my.salesforce.com/")
-					.connectTimeout(20000)  
-					.socketTimeout(20000)
-//				.addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393")
-					.execute()
-					.returnResponse();
-			
-			//查看所有header			
-			System.out.println(httpResponse.getStatusLine());			
-			headers = httpResponse.getAllHeaders();			
-			for (Header h : headers)
-				System.out.println(h.getName() + " ===> " + h.getValue());			
-			System.out.println(line);
-//			//检查登录是否成功，即看redirect的地址有没有问题
-//			headers = httpResponse.getHeaders("location");
-//			System.out.println("location: " + headers[0].getValue());
-			
-			//打印Cookie
-			headers = httpResponse.getHeaders("Set-Cookie");			
-			if (headers.length <= 0) {
-				System.out.println("未获得cookie有误，请核实!");
-				return;			
-			}else{
-				text = headers[0].getValue();
-				System.out.println("Set-Cookie: "+text);
-				System.out.println(line);
-				cookie = text.split(";", 2)[0]; // 拿BrowserID
-				System.out.println(cookie);
-			}
-			
-			//设置登录参数
-			String pw = readInputLine("Please input your Salesforce password"); //从键盘读取密码
-			forms = Form.form()
-					.add("un", "shawn.peng@gmimarkets.com")
-					.add("hasRememberUn", "true")
-					.add("useSecure", "true")
-					.add("username", "shawn.peng@gmimarkets.com")
-					.add("pw", pw)
-					.add("Login", "登录")
-					.build();
-			
-//			cookie = "BrowserId=Pi2EjrVTQkSI-sAjoTXXgQ";
-			//登陆拿Cookie		
-	        httpResponse = Request.Post("https://gmi.my.salesforce.com/")  
-			        .connectTimeout(20000)
-			        .socketTimeout(20000)
-			        .addHeader("Cookie", cookie)
-			        .bodyForm(forms, Consts.UTF_8)
-			        .execute()
-			        .returnResponse();
-			
-			//拿Cookie
-			System.out.println(httpResponse.getStatusLine());			
-			headers = httpResponse.getAllHeaders();			
-			for (Header h : headers)
-				System.out.println(h.getName() + " ===> " + h.getValue());			
-			System.out.println(line);
-			//看Entity
-			text = EntityUtils.toString(httpResponse.getEntity(), Consts.UTF_8);
-			System.out.println(text);
-			//检查登录是否成功，即看redirect的地址有没有问题			
-			headers = httpResponse.getHeaders("location");
-			System.out.println("☆☆☆☆☆☆☆☆☆☆☆ 登陆成功！☆☆☆☆☆☆☆☆☆☆☆");
-			System.out.println("location: " + headers[0].getValue());
-//			if (headers[0].getValue().split("?", 2)[0] != "https://gmi.my.salesforce.com/secur/frontdoor.jsp"){
-//				System.out.println("登陆失败，返回地址为： " + headers[0].getValue() + "\r\n请检查！") ;
-//				return;
+//			// 拿登陆使用的cookie
+//			httpResponse = Request.Get("https://gmi.my.salesforce.com/")
+//					.connectTimeout(20000)  
+//					.socketTimeout(20000)
+////				.addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393")
+//					.execute()
+//					.returnResponse();
+//			
+//			//查看所有header			
+//			System.out.println(httpResponse.getStatusLine());			
+//			headers = httpResponse.getAllHeaders();			
+//			for (Header h : headers)
+//				System.out.println(h.getName() + " ===> " + h.getValue());			
+//			System.out.println(line);
+////			//检查登录是否成功，即看redirect的地址有没有问题
+////			headers = httpResponse.getHeaders("location");
+////			System.out.println("location: " + headers[0].getValue());
+//			
+//			//打印Cookie
+//			headers = httpResponse.getHeaders("Set-Cookie");			
+//			if (headers.length <= 0) {
+//				System.out.println("未获得cookie有误，请核实!");
+//				return;			
+//			}else{
+//				text = headers[0].getValue();
+//				System.out.println("Set-Cookie: "+text);
+//				System.out.println(line);
+//				cookie = text.split(";", 2)[0]; // 拿BrowserID
+//				System.out.println(cookie);
 //			}
-			//打印Cookie
-			headers = httpResponse.getHeaders("Set-Cookie");
-			for(Header h:headers){
-				text = h.getName() + " ===> " + h.getValue();
-				System.out.println(text);
-				cookie = cookie + "; " + h.getValue().split(";", 2)[0];
-				if (headers.length != 5) {
-					System.out.println("cookie个数有误，登陆可能未成功，请核实!");
-//					return;
-				}			
-			}
-			System.out.println(line);			
-			System.out.println("Set-Cookie: " + cookie);
-			System.out.println(line);
+//			
+//			//设置登录参数
+//			String pw = readInputLine("Please input your Salesforce password"); //从键盘读取密码
+//			forms = Form.form()
+//					.add("un", "shawn.peng@gmimarkets.com")
+//					.add("hasRememberUn", "true")
+//					.add("useSecure", "true")
+//					.add("username", "shawn.peng@gmimarkets.com")
+//					.add("pw", pw)
+//					.add("Login", "登录")
+//					.build();
+//			
+////			cookie = "BrowserId=Pi2EjrVTQkSI-sAjoTXXgQ";
+//			//登陆拿Cookie		
+//	        httpResponse = Request.Post("https://gmi.my.salesforce.com/")  
+//			        .connectTimeout(20000)
+//			        .socketTimeout(20000)
+//			        .addHeader("Cookie", cookie)
+//			        .bodyForm(forms, Consts.UTF_8)
+//			        .execute()
+//			        .returnResponse();
+//			
+//			//拿Cookie
+//			System.out.println(httpResponse.getStatusLine());			
+//			headers = httpResponse.getAllHeaders();			
+//			for (Header h : headers)
+//				System.out.println(h.getName() + " ===> " + h.getValue());			
+//			System.out.println(line);
+//			//看Entity
+//			text = EntityUtils.toString(httpResponse.getEntity(), Consts.UTF_8);
+//			System.out.println(text);
+//			//检查登录是否成功，即看redirect的地址有没有问题			
+//			headers = httpResponse.getHeaders("location");
+//			System.out.println("☆☆☆☆☆☆☆☆☆☆☆ 登陆成功！☆☆☆☆☆☆☆☆☆☆☆");
+//			System.out.println("location: " + headers[0].getValue());
+////			if (headers[0].getValue().split("?", 2)[0] != "https://gmi.my.salesforce.com/secur/frontdoor.jsp"){
+////				System.out.println("登陆失败，返回地址为： " + headers[0].getValue() + "\r\n请检查！") ;
+////				return;
+////			}
+//			//打印Cookie
+//			headers = httpResponse.getHeaders("Set-Cookie");
+//			for(Header h:headers){
+//				text = h.getName() + " ===> " + h.getValue();
+//				System.out.println(text);
+//				cookie = cookie + "; " + h.getValue().split(";", 2)[0];
+//				if (headers.length != 5) {
+//					System.out.println("cookie个数有误，登陆可能未成功，请核实!");
+////					return;
+//				}			
+//			}
+//			System.out.println(line);			
+//			System.out.println("Set-Cookie: " + cookie);
+//			System.out.println(line);
 			
-//			cookie = "BrowserId=bMnWukcOSNivcwsugMHDCw; inst=APP_6F; rememberUn=false; login=; com.salesforce.LocaleInfo=us; oinfo=\"c3RhdHVzPUFDVElWRSZ0eXBlPTImb2lkPTAwRDkwMDAwMDAwWmNtVA==\"";			
+			cookie = readInputLine("键入cookie");			
 			// 登陆成功后要开始搞事情了
-			response = Request.Get("https://gmi.my.salesforce.com/_ui/core/chatter/ui/ChatterPage")  
-			        .connectTimeout(10000)  
-			        .socketTimeout(10000)
-			        .addHeader("Cookie", cookie)
-			        .execute();
-			        
-//			text = response.returnResponse().getStatusLine().toString();
-//			System.out.println(text + "\r\n" + line);
-			text = response.returnContent().asString(Consts.UTF_8);
-			
-			System.out.println(text);
-			System.out.println(line);
+//			response = Request.Get("https://gmi.my.salesforce.com/_ui/core/chatter/ui/ChatterPage")  
+//			        .connectTimeout(10000)  
+//			        .socketTimeout(10000)
+//			        .addHeader("Cookie", cookie)
+//			        .execute();
+//			        
+////			text = response.returnResponse().getStatusLine().toString();
+////			System.out.println(text + "\r\n" + line);
+//			text = response.returnContent().asString(Consts.UTF_8);
+//			
+//			System.out.println(text);
+//			System.out.println(line);
 			
 			// 拿entityID, sysMod, 和ConfirmationToken
 			String caseUrl = "https://gmi.my.salesforce.com?ec=302&startURL=%2F5006F00001IA7KE";
@@ -211,8 +211,8 @@ public class salesforce {
 			String confirmToken = text.split("_CONFIRMATIONTOKEN=", 2)[1]
 					.split("&", 2)[0];
 			String sysMod = text.split("\"sysMod\":\"", 2)[1]
-					.split("\"});")[0];
-			String entityId = caseUrl.split("gmi.my.salesforce.com?ec=302&startURL=%2F")[1];
+					.split("\"}", 2)[0];
+			String entityId = caseUrl.split("startURL=%2F")[1];
 			System.out.println("ConfirmationToken, sysMod, entityId:");
 			System.out.println(confirmToken);
 			System.out.println(sysMod);
@@ -235,10 +235,12 @@ public class salesforce {
 					.add("cas7", "On Hold")
 					.add("cas14", "Deposit") // subject
 					.add("00N6F00000EfzPF", depositDetailsMap.get("Confirmed"))
+					.add("00N90000005QT62", depositDetailsMap.get("Confirmed"))
 					.add("00N90000005QUg3", depositDetailsMap.get("通过"))
 					.add("00N90000005QUii", depositDetailsMap.get("Actual Received"))
 					.add("00N90000005RqF1", "宝付支付")	
 					.add("00N90000005QRqn", depositDetailsMap.get("Deposit Amount"))
+					.add("cas16", depositDetailsMap.get("Account Input Reference / Notes"))
 					.build();
 			
 			httpResponse = Request.Post("https://gmi.my.salesforce.com/ui/common/InlineEditEntitySave")
@@ -256,21 +258,21 @@ public class salesforce {
 			System.out.println(EntityUtils.toString(httpResponse.getEntity(), Consts.UTF_8));
 			System.out.println(line);
 			
-			//退出登录
-			httpResponse = Request.Get("https://gmi.my.salesforce.com/secur/logout.jsp")
-			            .addHeader("Cookie", cookie)
-			            .execute()
-			            .returnResponse();
-			
-			//查看headers
-			System.out.println("☆☆☆☆☆☆☆☆☆☆☆ 退出登录！ ☆☆☆☆☆☆☆☆☆☆☆");
-			System.out.println(httpResponse.getStatusLine());			
-			headers = httpResponse.getAllHeaders();			
-			for (Header h : headers)
-				System.out.println(h.getName() + " ===> " + h.getValue());			
-			System.out.println(line);
-			text = EntityUtils.toString(httpResponse.getEntity(), Consts.UTF_8);
-			System.out.println(text);
+//			//退出登录
+//			httpResponse = Request.Get("https://gmi.my.salesforce.com/secur/logout.jsp")
+//			            .addHeader("Cookie", cookie)
+//			            .execute()
+//			            .returnResponse();
+//			
+//			//查看headers
+//			System.out.println("☆☆☆☆☆☆☆☆☆☆☆ 退出登录！ ☆☆☆☆☆☆☆☆☆☆☆");
+//			System.out.println(httpResponse.getStatusLine());			
+//			headers = httpResponse.getAllHeaders();			
+//			for (Header h : headers)
+//				System.out.println(h.getName() + " ===> " + h.getValue());			
+//			System.out.println(line);
+//			text = EntityUtils.toString(httpResponse.getEntity(), Consts.UTF_8);
+//			System.out.println(text);
 			
 			
 		} catch (ClientProtocolException e) {
