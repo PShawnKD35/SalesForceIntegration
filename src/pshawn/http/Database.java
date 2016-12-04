dd812ecb0e81746610e18ce96c13a9c449332fa9
Shawn Peng <lankeypeng@163.com>
2016-12-04 21:14:25 +0800

    add MySQL test class

--- src/pshawn/http/Database.java
+++ src/pshawn/http/Database.java
@@ -1,20 +1,80 @@
-/**
- * 
- */
 package pshawn.http;
 
-/**
+import java.sql.*;
+
+/**测试数据库的类
  * @author Shawn Peng
  *
  */
 public class Database {
 
+    // JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String DB_URL = "jdbc:mysql://localhost:3306/world";
+ 
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "456456";
+    
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            Class.forName("com.mysql.jdbc.Driver");
+        
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        
+            // 执行查询
+            System.out.println(" 实例化Statement对...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM city WHERE population>10000000 LIMIT 100";
+            ResultSet rs = stmt.executeQuery(sql);
+        
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                int id  = rs.getInt("id");
+                String name = rs.getString("name");
+                int population = rs.getInt("population");
+    
+                // 输出数据
+                System.out.print("ID: " + id);
+                System.out.print(", 城市: " + name);
+                System.out.print(", 人口: " + population);
+                System.out.print("\r\n");
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
+		
 	}
 
 }
